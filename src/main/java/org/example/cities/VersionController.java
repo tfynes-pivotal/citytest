fbc35de35e49417bc42f7ffcd9132b9e2dd190e7
Mark Fynes <tmfynes@yahoo.com>
2014-12-04 09:06:51 -0500

    dot added

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Dallas Roadshow!";
+        return "Greetings Dallas Roadshow!.";
     }
 }
