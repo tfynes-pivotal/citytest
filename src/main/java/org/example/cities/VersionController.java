1de46e9ebd7bb9d87ada5f3e771346d55719aaea
Mark Fynes <tmfynes@yahoo.com>
2014-12-04 14:30:01 -0500

    blah2

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Toronto Roadshow!";
+        return "Greetings Toronto Roadshow!...";
     }
 }
